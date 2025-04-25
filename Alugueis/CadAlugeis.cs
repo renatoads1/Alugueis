ef1270c7b3bda1e043b7a14ae5c88e3e354ae724
renatoads1 <renato.lacerda>
2025-04-25 16:56:35 -0300

    Adicionar arquivos de projeto.

---
+++ Alugueis/CadAlugeis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alugueis
+{
+    public class CadAlugeis
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string NumeroQuarto { get; set; }
+        public double Aluguel { get; set; }
+
+    }
+}
---
+++ Alugueis/Program.cs
@@ -0,0 +1,22 @@
+﻿using Alugueis;
+
+Console.WriteLine("digite a quantidade de quartos a ser alugado");
+int quartos = int.Parse(Console.ReadLine());
+CadAlugeis[] alug = new CadAlugeis[quartos];
+for (int i=0;i<quartos;i++) 
+{
+
+    Console.WriteLine("digite o nome");
+    alug[i].Nome = Console.ReadLine();
+    Console.WriteLine("digite o email");
+    alug[i].Email = Console.ReadLine();
+    Console.WriteLine("digite o numero do quarto");
+    alug[i].NumeroQuarto = Console.ReadLine();
+    Console.WriteLine("digite o valor do aluguel");
+    alug[i].Aluguel = double.Parse(Console.ReadLine());
+}
+Console.WriteLine("Alugueis cadastrados:");
+for (int i = 0; i < quartos; i++)
+{
+    Console.WriteLine($"Nome: {alug[i].Nome}, Email: {alug[i].Email}, Quarto: {alug[i].NumeroQuarto}, Aluguel: {alug[i].Aluguel}");
+}
