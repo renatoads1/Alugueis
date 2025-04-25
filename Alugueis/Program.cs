ee00c0745b51a3cf9ec89574e84eb64180ecc5e6
renatoads1 <renato.lacerda>
2025-04-25 16:58:44 -0300

    correção

--- Alugueis/Program.cs
+++ Alugueis/Program.cs
@@ -2,21 +2,21 @@
 
 Console.WriteLine("digite a quantidade de quartos a ser alugado");
 int quartos = int.Parse(Console.ReadLine());
-CadAlugeis[] alug = new CadAlugeis[quartos];
+CadAlugeis alug = new CadAlugeis();
 for (int i=0;i<quartos;i++) 
 {
 
     Console.WriteLine("digite o nome");
-    alug[i].Nome = Console.ReadLine();
+    alug.Nome = Console.ReadLine();
     Console.WriteLine("digite o email");
-    alug[i].Email = Console.ReadLine();
+    alug.Email = Console.ReadLine();
     Console.WriteLine("digite o numero do quarto");
-    alug[i].NumeroQuarto = Console.ReadLine();
+    alug.NumeroQuarto = Console.ReadLine();
     Console.WriteLine("digite o valor do aluguel");
-    alug[i].Aluguel = double.Parse(Console.ReadLine());
+    alug.Aluguel = double.Parse(Console.ReadLine());
 }
 Console.WriteLine("Alugueis cadastrados:");
 for (int i = 0; i < quartos; i++)
 {
-    Console.WriteLine($"Nome: {alug[i].Nome}, Email: {alug[i].Email}, Quarto: {alug[i].NumeroQuarto}, Aluguel: {alug[i].Aluguel}");
+    Console.WriteLine($"Nome: {alug.Nome}, Email: {alug.Email}, Quarto: {alug.NumeroQuarto}, Aluguel: {alug.Aluguel}");
 }
